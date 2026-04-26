1afad5b833f413cb83f0a360be4363df2808fad9
Indah nurhayati <indahindah1233162@gmail.com>
2026-04-26 10:37:17 -0400

    Update profile.blade.php

--- profile.blade.php
+++ profile.blade.php
@@ -2,14 +2,18 @@
 
 @section('content')
     <h2>Profile</h2>
+
+    <x-alert color="blue">
+        Ini halaman profile
+    </x-alert>
+
     <p>Nama: {{ $nama }}</p>
     <p>Email: {{ $email }}</p>
     <p>Alamat: {{ $alamat }}</p>
 @endsection
 
-@push('script')
+@push('scripts')
 <script>
-    console.log('Halaman Profile dibuka');
-    alert('Selamat datang di halam profile');
+    console.log('Halaman profile dibuka');
 </script>
 @endpush
